b4c0151501731a7631c51fe2b2d5f113a2a429d1
noaheakin <noaheakin@gmail.com>
2020-10-06 16:33:02 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const newForm = document.querySelector('#create-task-form');
+  const newTaskDes = document.querySelector('#new-task-description');
+  const list = document.querySelector('#tasks')
+  newForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+   const itemLi = document.createElement('li')
+   itemLi.innerText = newTaskDes.value
+   list.appendChild(itemLi) 
+  })
 });
